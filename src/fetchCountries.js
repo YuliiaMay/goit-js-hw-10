92ecf670b9feee5e772fc3bac937365e3358ba82
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-02-07 16:39:16 +0200

    res-07/02

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,6 +3,11 @@ const FILTER_RESPONSE = 'fields=name,capital,population,flags,languages';
 
 export function fetchCountries(name) {
     return fetch(`${BASE_URL}${name}?${FILTER_RESPONSE}`)
-    .then(response => response.json())
+    .then(response => {
+        if (response.status === 404) {
+            throw new Error(response.status);
+        }
+        return response.json();
+        })
     .catch(error => console.error);
 }
